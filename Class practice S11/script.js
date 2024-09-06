23528c0f85b4a2023da37ccb01599abab1c72dc4
Mobina SaghaAbdollahi <Mobinaabdollahi20@gmail.com>
2024-09-06 08:41:51 +0330

    Add files via upload

---
+++ Class practice S11/script.js
@@ -0,0 +1,15 @@
+const inputText=document.querySelector(".inputText");
+const itemContainer=document.querySelector(".list");
+const clickHandler = (event) => {
+const newElement=document.createElement("li");
+newElement.classList.add("listItem");
+newElement.innerText =event.target.value;
+event.target.value="";
+itemContainer.appendChild(newElement);
+const newElementButton=document.createElement("button");
+newElementButton.classList.add("remove");
+newElementButton.innerText =" X ";
+newElement.appendChild(newElementButton);
+const deleteItemHandler = () => itemContainer.removeChild(newElement);
+newElementButton.addEventListener("click",deleteItemHandler);}
+inputText.addEventListener("change", clickHandler);
